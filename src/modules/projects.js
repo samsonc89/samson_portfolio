d4c5de96b752786e02d63ed78c75d6d6ba3550da
Samson Cheung <samsonc89@yahoo.com>
2023-02-08 14:26:32 -0800

    Update

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -29,7 +29,7 @@ function createProjectCard(project) {
   thumbnailImage.classList.add("thumbnail-image");
   thumbnailImage.src = images[project.thumbnail];
 
-  const projectTitle = document.createElement("h3");
+  const projectTitle = document.createElement("h2");
   projectTitle.classList.add("project-title");
   projectTitle.textContent = project.title;
 
@@ -44,17 +44,26 @@ function createProjectCard(project) {
   gitWrapper.classList.add("git-wrapper");
   const gitLink = document.createElement("a");
   gitLink.href = project.code;
-  const gitIcon = document.createElement("img");
+  gitLink.setAttribute("target", "_blank");
+  const gitIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   gitIcon.classList.add("git-icon", "icon");
-  gitIcon.src = gitSVG;
+  gitIcon.setAttribute("viewBox", "0 0 100 100");
+  gitIcon.innerHTML =
+    '<path d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"/>';
 
   const liveWrapper = document.createElement("div");
   liveWrapper.classList.add("live-wrapper");
   const liveLink = document.createElement("a");
   liveLink.href = project.live;
-  const liveIcon = document.createElement("img");
+  liveLink.setAttribute("target", "_blank");
+  const liveIcon = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg"
+  );
   liveIcon.classList.add("live-icon", "icon");
-  liveIcon.src = liveSVG;
+  liveIcon.setAttribute("viewBox", "0 0 21 21");
+  liveIcon.innerHTML = `<path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" />
+  `;
 
   gitLink.appendChild(gitIcon);
   liveLink.appendChild(liveIcon);
